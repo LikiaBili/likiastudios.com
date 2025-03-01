b2bffc6acbc500813f8a7fc31b22485595f14564
Likia <likiawork@163.com>
2025-03-01 17:31:33 +0800

    Beta V0.2.0 Deployment 9 Release.

--- app/(website)/article/[bid]/page.tsx
+++ app/(website)/article/[bid]/page.tsx
@@ -11,7 +11,7 @@ import path from "path";
 export default async function Page({params} : {params : {bid : string}}){
     let devlog = findDevlog(params.bid);
     let userLanguage : "en_us" | "zh_cn" = "en_us";
-    let pageText = await fs.readFile(path.join(process.cwd(), 'article/'+devlog['id']+'/' + devlog["document"][userLanguage]), 'utf8');
+    let pageText = await fs.readFile(path.resolve(process.cwd()+'/article/'+devlog['id']+'/' + devlog["document"][userLanguage]), 'utf8');
     function render(){
         return (
                 <Markdown options={{
